e772ab59dd16b3fa0b7aec529281efd598326392
dawn-microna <tommy.sargaison@hotmail.com>
2026-05-11 22:34:12 +0100

    basic loop set up

--- esp32-platformIO-project/src/main.cpp
+++ esp32-platformIO-project/src/main.cpp
@@ -4,17 +4,25 @@
 
 #include "driver/temp_sensor.h" // driver to read CPU core temp
 
-// LED SETUP CONSTS
+// LED SETUP
 const int red = 9;
 const int yellow = 10;
 const int green = 11;
 const int button = 5;
 
-// WIFI SETUP CONSTS
+// WIFI SETUP
 const char* ssid = "TNCAP1E424B";
 const char* password = "CmtkkYCnf42rJqMc";
 const char* host = ""; // where we push these post requests to
 const int httpPort = 80;
+
+// LOOP FUNCTIONS
+void sendSensorData(); // could change the return types
+void getSensorData();
+
+void receiveRequests(); // poll for light changes - something that requires an interrupt of some kind?
+
+void changeLights();
 
 
 void setup() {
@@ -37,15 +45,30 @@ void setup() {
     delay(800);
     Serial.print(".");
   }
-  Serial.printf("\nConnected to Wifi network %s\n", WiFi.SSID());
+  Serial.printf("\nConnected to Wifi network %s at IP %s\n", WiFi.SSID(), WiFi.localIP());
 }
 
 void loop() {
-  // CODE TO GET A CPU TEMP SENSOR READING
-  float cpuTemp = 0;
-  temp_sensor_read_celsius(&cpuTemp);
-  delete(&cpuTemp); // dont think this is required
 
   // hwefoofsjiojgwg ill set up http connections and breaking all this code out into separate fns later
 }
 
+void sendSensorData() {
+  // for this we need to define what we are sending
+}
+void getSensorData() {
+  // CHECK IF BUTTON IS LOW OR HIGH
+
+  // CODE TO GET A CPU TEMP SENSOR READING
+  float cpuTemp = 0;
+  temp_sensor_read_celsius(&cpuTemp);
+  delete(&cpuTemp); // dont think this is required
+}
+
+void receiveRequests() {
+
+}
+
+void changeLights() {
+
+}
